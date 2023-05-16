2202ff7288c9cb319311a776735c599b91a11870
Abin-98 <abinvarkey01998@gmail.com>
2023-05-16 11:03:48 +0530

    changes1

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
 
 const form=document.querySelector("form")
 form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    form.classList.add('was-validated');
 
-    if(!form.checkValidity()){
-        e.preventDefault();
+    //storing data in object format
+
+    let obj={ 
+    nm:e.target.name.value,
+    em:e.target.email.value,
+    ph:e.target.phone.value,
+    dt:e.target.date.value,
+    tm:e.target.time.value
     }
-    form.classList.add('was-validated')
-    let nm=e.target.name.value;
-    let em=e.target.email.value;
-    let ph=e.target.phone.value;
-    let dt=e.target.date.value;
-    let tm=e.target.time.value;
-
-    localStorage.setItem("name", nm);
-    localStorage.setItem("email", em);
-    localStorage.setItem("phone", ph);
-    localStorage.setItem("date", dt);
-    localStorage.setItem("time", tm);
+    let obj_string=JSON.stringify(obj);
+    localStorage.setItem("data_object", obj_string);
 })
 
 
